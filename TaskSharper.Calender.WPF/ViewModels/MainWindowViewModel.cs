b64160f231a8d494fff7e5ef2792592ea8b50ff1
Andreas Alminde <alminde7@hotmail.com>
2017-09-05 11:07:58 +0200

    Fixed namespace for MainWindowViewModel

--- TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TaskSharper.Calender.WPF
+namespace TaskSharper.Calender.WPF.ViewModels
 {
     public class MainWindowViewModel : BindableBase 
     {
