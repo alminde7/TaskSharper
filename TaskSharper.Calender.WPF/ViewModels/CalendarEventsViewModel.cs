3070db358f1912bf9023e8bc2152f54ebfe60f1e
Andreas Alminde <alminde7@hotmail.com>
2017-09-06 14:02:36 +0200

    Renamed CalendarAppointment to CalendarEvent

---
+++ TaskSharper.Calender.WPF/ViewModels/CalendarEventsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TaskSharper.Calender.WPF.ViewModels
+{
+    public class CalendarEventsViewModel
+    {
+        IEnumerable<CalendarEventsViewModel> Appointments { get; set; }
+    }
+}
