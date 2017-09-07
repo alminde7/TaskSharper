aa4c1a9ce3e77b6d0b1c52f75594d7903b4df82c
Andreas Alminde <alminde7@hotmail.com>
2017-09-07 15:57:34 +0200

    Made DateHeaders i WeekView dynamic.

---
+++ TaskSharper.Calender.WPF/ViewModels/CalendarWeekViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace TaskSharper.Calender.WPF.ViewModels
+{
+    public class CalendarWeekViewModel : BindableBase
+    {
+        public ObservableCollection<CalendarDateViewModel> DateHeaders { get; set; }
+
+        public CalendarWeekViewModel()
+        {
+            DateHeaders = new ObservableCollection<CalendarDateViewModel>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                DateHeaders.Add(new CalendarDateViewModel(i));
+            }
+        }
+    }
+}
--- TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarDateViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarDateViewModel.cs
@@ -8,5 +8,25 @@ namespace TaskSharper.Calender.WPF.ViewModels
 
         public string DayOfWeek { get; set; } = DateTime.Now.DayOfWeek.ToString();
         public int DayOfMonth { get; set; } = DateTime.Now.Day;
+
+        public CalendarDateViewModel()
+        {
+            
+        }
+
+        public CalendarDateViewModel(int dayOfMonth = 0)
+        {
+            SetDate(dayOfMonth);
+        }
+
+        private void SetDate(int today)
+        {
+            var dayOffset = today - (int)DateTime.Now.DayOfWeek;
+
+            var dateTime = DateTime.Now.AddDays(dayOffset);
+
+            DayOfMonth = dateTime.Day;
+            DayOfWeek = dateTime.DayOfWeek.ToString();
+        }
     }
 }
