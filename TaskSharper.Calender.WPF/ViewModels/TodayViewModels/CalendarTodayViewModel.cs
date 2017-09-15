25f28f906caedadc9028771364d69f9f68fb4d4c
Andreas Alminde <alminde7@hotmail.com>
2017-09-15 21:49:01 +0200

    Changed viewModel properties in TodayView to not implement SetProperty from BindableBase

--- TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarTodayViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarTodayViewModel.cs
@@ -7,23 +7,11 @@ namespace TaskSharper.Calender.WPF.ViewModels
 {
     public class CalendarTodayViewModel : BindableBase
     {
-        private CalendarEventsViewModel _eventsViewModel;
-        private CalendarDateViewModel _dateViewModel;
         public IEventAggregator EventAggregator { get; }
         public ICalendarService CalendarService { get; }
 
-        public CalendarEventsViewModel EventsViewModel
-        {
-            get => _eventsViewModel;
-            set => SetProperty(ref _eventsViewModel, value);
-        }
-
-        public CalendarDateViewModel DateViewModel
-        {
-            get => _dateViewModel;
-            set => SetProperty(ref _dateViewModel, value);
-        }
-
+        public CalendarEventsViewModel EventsViewModel { get; set; }
+        public CalendarDateViewModel DateViewModel { get; set; }
         public DateTime CurrentDate { get; set; }
 
         public CalendarTodayViewModel(IEventAggregator eventAggregator, ICalendarService calendarService)
