71e7535b3e96b4e0e89bd13037f11e8ad2e50df9
Mads <madsm92@gmail.com>
2017-09-21 10:06:06 +0200

    TAS-51 #time 1h #comment Added popup message when clicking an event.

--- TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarEventViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarEventViewModel.cs
@@ -5,6 +5,8 @@ using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
+using Serilog;
+using TaskSharper.Calender.WPF.Events;
 using TaskSharper.Domain.Calendar;
 
 namespace TaskSharper.Calender.WPF.ViewModels
@@ -12,12 +14,16 @@ namespace TaskSharper.Calender.WPF.ViewModels
     public class CalendarEventViewModel : BindableBase
     {
         private readonly IRegionManager _regionManager;
-        
+        private readonly IEventAggregator _eventAggregator;
+        private readonly ILogger _logger;
+
         public DelegateCommand EventClickCommand { get; set; }
         
         private bool _isTitleAndDescriptionActivated;
         private Event _event;
-        
+        private bool _isPopupOpen;
+        SubscriptionToken _subscriptionToken;
+
 
         public int TimeOfDay { get; set; }
 
@@ -42,15 +48,30 @@ namespace TaskSharper.Calender.WPF.ViewModels
             get => _isTitleAndDescriptionActivated;
             set => SetProperty(ref _isTitleAndDescriptionActivated, value);
         }
+
+        public bool IsPopupOpen
+        {
+            get => _isPopupOpen;
+            set => SetProperty(ref _isPopupOpen, value);
+        }
         
-
-
-        public CalendarEventViewModel(int timeOfDay, IRegionManager regionManager)
+        public CalendarEventViewModel(int timeOfDay, IRegionManager regionManager, IEventAggregator eventAggregator, ILogger logger)
         {
             TimeOfDay = timeOfDay;
             IsTitleAndDescriptionActivated = true;
             _regionManager = regionManager;
+            _eventAggregator = eventAggregator;
+            _logger = logger;
             EventClickCommand = new DelegateCommand(EventClick, CanExecute);
+            _eventAggregator.GetEvent<EventClickedEvent>().Subscribe(LogEventClick);
+        }
+
+        private void LogEventClick(EventClickObject obj)
+        {
+            if (obj.Event != null)
+            {
+                _logger.Information($"Event {obj.Event.Title} (id: {obj.Event.Id}) with timespan {obj.TimeOfDay} was clicked at {DateTime.Now}");
+            }
         }
 
         private void Navigate(string uri)
@@ -60,12 +81,30 @@ namespace TaskSharper.Calender.WPF.ViewModels
 
         private void EventClick()
         {
-            Navigate("CalendarEventDetailsView");
+            
+            if (IsPopupOpen)
+            {
+                Navigate("CalendarEventDetailsView");
+            }
+            else
+            {
+                IsPopupOpen = true;
+                _subscriptionToken = _eventAggregator.GetEvent<EventClickedEvent>().Subscribe(ClosePopup);
+            }
         }
 
         private bool CanExecute()
         {
-            return !string.IsNullOrEmpty(Event.Id);
+            var canExecute = !string.IsNullOrEmpty(Event?.Id);
+            _eventAggregator.GetEvent<EventClickedEvent>().Publish(new EventClickObject() { Event = Event, TimeOfDay = TimeOfDay });
+
+            return canExecute;
+        }
+
+        private void ClosePopup(EventClickObject obj)
+        {
+            IsPopupOpen = false;
+            _eventAggregator.GetEvent<EventClickedEvent>().Unsubscribe(_subscriptionToken);
         }
     }
 }
--- TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarEventsViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/TodayViewModels/CalendarEventsViewModel.cs
@@ -63,7 +63,7 @@ namespace TaskSharper.Calender.WPF.ViewModels
         {
             for (int i = 0; i < HoursInADay; i++)
             {
-                CalendarEvents.Add(new CalendarEventViewModel(i, _regionManager));
+                CalendarEvents.Add(new CalendarEventViewModel(i, _regionManager, _eventAggregator));
             }
         }
 
