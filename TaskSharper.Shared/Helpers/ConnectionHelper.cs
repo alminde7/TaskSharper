18e17ca59d416d7685a358f9598d89def40f5f82
Andreas Alminde <alminde7@hotmail.com>
2017-09-25 20:23:56 +0200

    Added check to ensure connection to elasticsearch before connecting to it.

---
+++ TaskSharper.Shared/Helpers/ConnectionHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace TaskSharper.Shared.Helpers
+{
+    public class ConnectionHelper
+    {
+        public static bool CheckConnectionElasticsearch(string url)
+        {
+            var httpClient = new HttpClient();
+            var result = httpClient.GetAsync(url).Result;
+            return result.IsSuccessStatusCode;
+        }
+    }
+}
