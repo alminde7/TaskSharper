6c7ea8e5052d54a14600a3505b90225ff2401ed4
Jeppe Knoth <jeppeknoth@gmail.com>
2017-10-05 16:25:08 +0200

    Added functionality for Notification pop up

---
+++ TaskSharper.Calender.WPF/Events/NotificationEvents/NotificationEvent.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Events;
+using TaskSharper.Calender.WPF.Events.Resources;
+
+namespace TaskSharper.Calender.WPF.Events.NotificationEvents
+{
+    public class NotificationEvent : PubSubEvent<Notification>
+    {
+
+    }
+}
--- TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using Prism.Mvvm;
 using Prism.Regions;
 using Serilog;
 using TaskSharper.Calender.WPF.Events;
+using TaskSharper.Calender.WPF.Events.NotificationEvents;
 using TaskSharper.Calender.WPF.Events.Resources;
 using TaskSharper.Calender.WPF.Views;
 using TaskSharper.Domain.BusinessLayer;
@@ -17,8 +18,12 @@ namespace TaskSharper.Calender.WPF.ViewModels
         private IEventAggregator _eventAggregator;
         private readonly ILogger _logger;
         private bool _spinnerVisible;
+        private bool _isPopupOpen;
+        private string _notificationTitle;
+        private string _notificationMessage;
 
         public DelegateCommand<string> NavigateCommand { get; set; }
+        public DelegateCommand CloseNotificationCommand { get; set; }
 
         public MainWindowViewModel(IRegionManager regionManager, IEventAggregator eventAggregator, ILogger logger)
         {
@@ -27,8 +32,11 @@ namespace TaskSharper.Calender.WPF.ViewModels
             _logger = logger;
 
             _eventAggregator.GetEvent<SpinnerEvent>().Subscribe(SetSpinnerVisibility);
-
+            _eventAggregator.GetEvent<NotificationEvent>().Subscribe(ShowNotification);
             NavigateCommand = new DelegateCommand<string>(Navigate);
+            CloseNotificationCommand = new DelegateCommand(ClosePopUp);
+
+            IsPopupOpen = true;
         }
 
         private void Navigate(string uri)
@@ -36,10 +44,39 @@ namespace TaskSharper.Calender.WPF.ViewModels
             _regionManager.RequestNavigate("CalendarRegion", uri);
         }
 
+        private void ClosePopUp()
+        {
+            IsPopupOpen = false;
+        }
+
+        public bool IsPopupOpen
+        {
+            get => _isPopupOpen;
+            set => SetProperty(ref _isPopupOpen, value);
+        }
+
         public bool SpinnerVisible
         {
             get => _spinnerVisible;
             set => SetProperty(ref _spinnerVisible, value);
+        }
+
+        public string NotificationTitle
+        {
+            get => _notificationTitle = "Custom";
+            set => SetProperty(ref _notificationTitle, value);
+        }
+        public string NotificationMessage
+        {
+            get => _notificationMessage = "Paint job";
+            set => SetProperty(ref _notificationMessage, value);
+        }
+
+        private void ShowNotification(Notification notification)
+        {
+            NotificationTitle = notification.Title;
+            NotificationMessage = notification.Message;
+            IsPopupOpen = true;
         }
 
         private void SetSpinnerVisibility(EventResources.SpinnerEnum state)
