9c271a3f2f75600ac05bba8af4d9dae696d16bbf
Mads <madsm92@gmail.com>
2017-10-25 16:02:54 +0200

    TAS-76 #time 2h #comment Added functionality to scroll up and down buttons, and hiding them when in MonthView

--- TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
+++ TaskSharper.Calender.WPF/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,7 @@ using TaskSharper.Calender.WPF.Config;
 using TaskSharper.Calender.WPF.Events;
 using TaskSharper.Calender.WPF.Events.NotificationEvents;
 using TaskSharper.Calender.WPF.Events.Resources;
+using TaskSharper.Calender.WPF.Events.ScrollEvents;
 using TaskSharper.Calender.WPF.Views;
 using TaskSharper.Domain.BusinessLayer;
 using WPFLocalizeExtension.Engine;
@@ -22,6 +23,7 @@ namespace TaskSharper.Calender.WPF.ViewModels
         private IEventAggregator _eventAggregator;
         private readonly ILogger _logger;
         private bool _spinnerVisible;
+        private bool _scrollButtonsVisible;
         private bool _isPopupOpen;
         private string _notificationTitle;
         private string _notificationMessage;
@@ -29,6 +31,8 @@ namespace TaskSharper.Calender.WPF.ViewModels
         public DelegateCommand<string> NavigateCommand { get; set; }
         public DelegateCommand CloseNotificationCommand { get; set; }
         public DelegateCommand<string> ChangeLanguageCommand { get; set; }
+        public DelegateCommand ScrollUpCommand { get; set; }
+        public DelegateCommand ScrollDownCommand { get; set; }
 
         public MainWindowViewModel(IRegionManager regionManager, IEventAggregator eventAggregator, ILogger logger)
         {
@@ -43,13 +47,27 @@ namespace TaskSharper.Calender.WPF.ViewModels
 
             NavigateCommand = new DelegateCommand<string>(Navigate);
             ChangeLanguageCommand = new DelegateCommand<string>(ChangeLanguage);
+            ScrollUpCommand = new DelegateCommand(ScrollUp);
+            ScrollDownCommand = new DelegateCommand(ScrollDown);
             IsPopupOpen = false;
+            ScrollButtonsVisible = true;
+        }
+
+        private void ScrollUp()
+        {
+            _eventAggregator.GetEvent<ScrollUpEvent>().Publish();
+        }
+
+        private void ScrollDown()
+        {
+            _eventAggregator.GetEvent<ScrollDownEvent>().Publish();
         }
 
         public bool Toggle { get; set; }
 
         private void Navigate(string uri)
         {
+            ScrollButtonsVisible = uri != "CalendarMonthView";
             _regionManager.RequestNavigate(ViewConstants.REGION_Calendar, uri);
         }
 
@@ -79,6 +97,12 @@ namespace TaskSharper.Calender.WPF.ViewModels
         {
             get => _spinnerVisible;
             set => SetProperty(ref _spinnerVisible, value);
+        }
+
+        public bool ScrollButtonsVisible
+        {
+            get => _scrollButtonsVisible;
+            set => SetProperty(ref _scrollButtonsVisible, value);
         }
 
         public string NotificationTitle
