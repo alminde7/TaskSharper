98474d1920f6a4a0e1e10715004f1cff6e9bf5c0
Andreas Alminde <alminde7@hotmail.com>
2017-10-26 15:38:43 +0200

    Created Appointments and Tasks application with appropriate dependencies

---
+++ TaskSharper.Appointments.WPF/Bootstrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Prism.Regions;
+using Prism.Unity;
+using TaskSharper.Domain.Calendar;
+using TaskSharper.Domain.Notification;
+using TaskSharper.Service.NotificationClient;
+using TaskSharper.Service.NotificationClient.HubConnectionClient;
+using TaskSharper.Service.RestClient;
+using TaskSharper.Service.RestClient.Factories;
+using TaskSharper.Shared.Logging;
+using Microsoft.Practices.Unity;
+using RestSharp;
+using Serilog;
+
+namespace TaskSharper.Appointments.WPF
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            // Register views
+        
+
+            // Register other dependencies
+            var logger = LogConfiguration.ConfigureWPF();
+
+            // Singletons
+            Container.RegisterInstance(typeof(ILogger), logger);
+            Container.RegisterInstance(typeof(IRestClient), new RestClient());
+
+            // Not singletons
+            Container.RegisterType<IRestRequestFactory, RestRequestFactory>();
+            Container.RegisterType<IEventRestClient, EventRestClient>();
+
+            var hubConnectionClient = new HubConnectionProxy("http://localhost:8000");
+            Container.RegisterInstance(typeof(IHubConnectionProxy), hubConnectionClient);
+            Container.RegisterType<INotificationClient, NotificationClient>();
+        }
+    }
+}
