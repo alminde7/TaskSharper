6d5e35076072582e2d2a3e42ebcf94b2838597f1
Jeppe Knoth <jeppeknoth@gmail.com>
2017-11-03 13:17:42 +0100

    Added view and viewmodel

---
+++ TaskSharper.WPF.Common/Components/Notification/NotificationViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+using TaskSharper.WPF.Common.Events.Resources;
+
+namespace TaskSharper.WPF.Common.Components.Notification
+{
+    public class NotificationViewModel : BindableBase
+    {
+
+        private bool _isPopupOpen;
+        private string _notificationTitle;
+        private string _notificationMessage;
+        private NotificationTypeEnum _notificationType;
+
+        public DelegateCommand CloseNotificationCommand { get; set; }
+
+        public DelegateCommand<string> ChangeLanguageCommand { get; set; }
+
+        public NotificationViewModel()
+        {
+            
+        }
+    }
+}
