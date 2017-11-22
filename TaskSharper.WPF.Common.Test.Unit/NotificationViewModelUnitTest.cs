8f87ea18f48ee94af498fae06e856f07044c9499
Jeppe Knoth <jeppeknoth@gmail.com>
2017-11-22 11:14:28 +0100

    Added ViewModelunittest

---
+++ TaskSharper.WPF.Common.Test.Unit/NotificationViewModelUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using NSubstitute;
+using Prism.Events;
+using TaskSharper.WPF.Common.Components.Notification;
+using TaskSharper.WPF.Common.Events;
+using TaskSharper.WPF.Common.Events.NotificationEvents;
+
+namespace TaskSharper.WPF.Common.Test.Unit
+{
+    public class MockNotificationEvent : NotificationEvent { }
+
+    public class MockCultureEvent : CultureChangedEvent { }
+
+    [TestFixture]
+    public class NotificationViewModelUnitTest
+    {
+        private NotificationViewModel _notificationViewModel;
+        [SetUp]
+        public void SetUp()
+        {
+            var eventAggregator = Substitute.For<IEventAggregator>();
+            eventAggregator.GetEvent<NotificationEvent>().Returns(new MockNotificationEvent());
+            eventAggregator.GetEvent<CultureChangedEvent>().Returns(new MockCultureChangedEvent());
+            _notificationViewModel = new NotificationViewModel(eventAggregator);
+        }
+
+        [Test]
+        public void testConstructor()
+        {
+            _notificationViewModel.Category = "test";
+            Assert.AreEqual(true,true);
+        }
+
+    }
+}
